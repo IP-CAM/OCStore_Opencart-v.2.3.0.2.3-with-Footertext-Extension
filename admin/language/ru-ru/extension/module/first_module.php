b7be3b4ba4937ab5f101936fffe7970cdc60295b
ViktorSS7 <32570737+ViktorSS7@users.noreply.github.com>
2019-05-06 20:51:18 +0300

    create language/first_module.php

---
+++ admin/language/ru-ru/extension/module/first_module.php
@@ -0,0 +1,11 @@
+<?php
+
+$_['heading_title']    = 'FooterText';
+ 
+$_['text_module']      = 'Modules';
+$_['text_success']     = 'Success: You have modified "First Module" module!';
+$_['text_edit']        = 'Edit "First Module" Module';
+ 
+
+$_['entry_status']     = 'Status';
+ 
