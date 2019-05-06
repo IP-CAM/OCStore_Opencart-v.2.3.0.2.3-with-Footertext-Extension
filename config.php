815e110274881ae21b75d999007f5c99efc58185
ViktorSS7 <32570737+ViktorSS7@users.noreply.github.com>
2019-05-06 20:45:26 +0300

    Initial commit

---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/');
+
+// DIR
+define('DIR_APPLICATION', 'W:/domains/localhost/catalog/');
+define('DIR_SYSTEM', 'W:/domains/localhost/system/');
+define('DIR_IMAGE', 'W:/domains/localhost/image/');
+define('DIR_LANGUAGE', 'W:/domains/localhost/catalog/language/');
+define('DIR_TEMPLATE', 'W:/domains/localhost/catalog/view/theme/');
+define('DIR_CONFIG', 'W:/domains/localhost/system/config/');
+define('DIR_CACHE', 'W:/domains/localhost/system/storage/cache/');
+define('DIR_DOWNLOAD', 'W:/domains/localhost/system/storage/download/');
+define('DIR_LOGS', 'W:/domains/localhost/system/storage/logs/');
+define('DIR_MODIFICATION', 'W:/domains/localhost/system/storage/modification/');
+define('DIR_UPLOAD', 'W:/domains/localhost/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'ocstore');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
